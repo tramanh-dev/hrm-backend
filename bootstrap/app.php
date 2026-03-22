02e8fad5be01d6b4a682cc1598049159deb2dcba
Tram Anh <trammanh278@gmail.com>
2026-03-22 22:32:37 +0700

    a

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,5 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        $exceptions->shouldRenderJsonWhen(function ($request, $e) {
+            if ($request->is('api/*')) {
+                return true;
+            }
+
+            return $request->expectsJson();
+        });
+    });
